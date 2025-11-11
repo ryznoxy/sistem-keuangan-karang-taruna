dbc00f102ab8f115607a3cdfa4dabb0e96d1aaa0
ryznoxy <rezajelek2@gmail.com>
2025-11-11 19:10:13 +0700

    Refactor footer and sidebar layout for improved structure and styling

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,13 @@
-        </main>
+  </main>
 
-        <footer class="bg-gray-900 text-white py-4 rounded-md">
-          <div class="container mx-auto px-4 text-center text-sm">
-            <p>&copy; <?= date('Y') ?> SIKAT. Reza</p>
-          </div>
-        </footer>
-        </div>
-        </div>
-        </body>
+  <footer class="bg-gray-900 text-white py-4 rounded-md fixed bottom-4 left-72 right-4 z-[999]">
+    <div class="container mx-auto px-4 text-center text-sm">
+      <p>&copy; <?= date('Y') ?> SIKAT. Reza</p>
+    </div>
+  </footer>
+  </div>
+  </div>
+  </div>
+  </body>
 
-        </html>
+  </html>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,35 +18,39 @@ $role = getUserRole();
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="flex flex-row">
-  <header class="fixed left-0 top-0 min-h-screen h-full w-64 bg-white border-r">
-    <div class="flex flex-col h-full justify-between ">
-      <div>
-        <h1 class="text-2xl font-bold px-4 pt-4">SIKAT</h1>
-        <nav>
-          <ul class="flex flex-col space-y-4 p-4">
-            <?php if ($role == 'admin' || $role == 'bendahara' || $role == 'ketua'): ?>
-              <li><a href="dashboard.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'dashboard.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Dashboard</a></li>
-              <li><a href="pemasukan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'pemasukan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Pemasukan</a></li>
-              <li><a href="pengeluaran.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'pengeluaran.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Pengeluaran</a></li>
-              <li><a href="laporan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'laporan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Laporan</a></li>
-              <li><a href="riwayat.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'riwayat.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Riwayat</a></li>
-              <?php if ($role == 'admin'): ?>
-                <li><a href="user.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'user.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Akun User</a></li>
+<body class="flex flex-col">
+  <div class="ml-64 border-b border-gray-300">
+    <h1 class="text-2xl font-bold px-4 py-4">Selamat datang, <?= $role ?></h1>
+  </div>
+  <div class="flex flex-row">
+    <header class="fixed left-0 top-0 min-h-screen h-full w-64 bg-white border-r border-gray-300">
+      <div class="flex flex-col h-full justify-between ">
+        <div>
+          <h1 class="text-2xl font-bold px-4 pt-4">SIKAT</h1>
+          <nav>
+            <ul class="flex flex-col space-y-4 p-4">
+              <?php if ($role == 'admin' || $role == 'bendahara' || $role == 'ketua'): ?>
+                <li><a href="dashboard.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'dashboard.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Dashboard</a></li>
+                <li><a href="pemasukan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'pemasukan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Pemasukan</a></li>
+                <li><a href="pengeluaran.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'pengeluaran.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Pengeluaran</a></li>
+                <li><a href="laporan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'laporan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Laporan</a></li>
+                <li><a href="riwayat.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'riwayat.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Riwayat</a></li>
+                <?php if ($role == 'admin'): ?>
+                  <li><a href="user.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'user.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Akun User</a></li>
+                <?php endif; ?>
+              <?php else: ?>
+                <li><a href="dashboard.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'dashboard.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Dashboard</a></li>
+                <li><a href="laporan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'laporan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Laporan</a></li>
+                <li><a href="riwayat.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'riwayat.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Riwayat</a></li>
               <?php endif; ?>
-            <?php else: ?>
-              <li><a href="dashboard.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'dashboard.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Dashboard</a></li>
-              <li><a href="laporan.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'laporan.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Laporan</a></li>
-              <li><a href="riwayat.php" class="block py-3 px-2 border rounded-md text-sm hover:bg-gray-200 <?php if (strpos($path, 'riwayat.php') !== false) echo 'bg-gray-800 text-white hover:bg-gray-900'; ?> transition-all ease-in-out">Riwayat</a></li>
-            <?php endif; ?>
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        </div>
+        <div class="flex flex-col items-start px-4 pb-4 space-y-2">
+          <p class="text-sm  inline-flex flex-col">Masuk sebagai : <span><?= $_SESSION['name'] ?> (<?= ucfirst($_SESSION['role']) ?>)</span></p>
+          <a href="../auth/logout.php" class="border px-3 py-2 rounded-md text-sm w-full text-center bg-gray-800 hover:bg-gray-700 text-white">Logout</a>
+        </div>
       </div>
-      <div class="flex flex-col items-start px-4 pb-4 space-y-2">
-        <p class="text-sm  inline-flex flex-col">Masuk sebagai : <span><?= $_SESSION['name'] ?> (<?= ucfirst($_SESSION['role']) ?>)</span></p>
-        <a href="../auth/logout.php" class="border px-3 py-2 rounded-md text-sm w-full text-center bg-gray-800 hover:bg-gray-700 text-white">Logout</a>
-      </div>
-    </div>
-    </div>
+  </div>
   </header>
-  <div class="fixed left-72 top-4 w-full max-w-[calc(100%-18rem)] pr-4">
+  <div class="fixed left-72 top-20 w-full max-w-[calc(100%-18rem)] pr-4">
